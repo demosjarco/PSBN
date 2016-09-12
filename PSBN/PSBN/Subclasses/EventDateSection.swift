41fabe815a94ab701afe0ec0d6a7245082949f75
Victor Ilisei <vilisei@gmail.com>
2016-09-11 18:04:59 -0700

    Created event date section subclass and theater list cell

---
+++ PSBN/PSBN/Subclasses/EventDateSection.swift
@@ -0,0 +1,16 @@
+//
+//  EventDateSection.swift
+//  PSBN
+//
+//  Created by Victor Ilisei on 9/11/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class EventDateSection: NSObject {
+    var year:Int = 0
+    var month:Int = 0
+    var day:Int = 0
+    var events = [[String:AnyObject]]()
+}
---
+++ PSBN/PSBN/Subclasses/TheaterListCell.swift
@@ -0,0 +1,24 @@
+//
+//  TheaterListCell.swift
+//  PSBN
+//
+//  Created by Victor Ilisei on 9/11/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class TheaterListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
