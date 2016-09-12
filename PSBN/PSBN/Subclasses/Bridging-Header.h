4952b3a0831f49805218700ecb5dc63974810509
Victor Ilisei <vilisei@gmail.com>
2016-09-11 18:26:08 -0700

    Added bridging header

---
+++ PSBN/PSBN/Subclasses/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  PSBN
+//
+//  Created by Victor Ilisei on 9/11/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "AFNetworking/UIImageView+AFNetworking.h"
+
+#endif /* Bridging_Header_h */
